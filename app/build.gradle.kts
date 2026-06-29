298094dd0106e9b528dfe9cb20906fff723eff6a
amynrsw <amynrsw@example.com>
2026-06-30 03:14:57 +0330

    fix: AGP 8.9.1, Gradle 8.11.1, compileSdk 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.v2ray.ang"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.v2ray.ang"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 735
         versionName = "2.2.5"
         multiDexEnabled = true
